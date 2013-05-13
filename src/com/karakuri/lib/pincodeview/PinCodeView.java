fe28b7f7a8fac562dd9c28facb191dba264b558a
Karakuri <karakuri.dev@gmail.com>
2013-05-12 20:28:22 -0700

    Handle ime editor actions

--- src/com/karakuri/lib/pincodeview/PinCodeView.java
+++ src/com/karakuri/lib/pincodeview/PinCodeView.java
@@ -16,6 +16,7 @@ import android.text.method.KeyListener;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.view.inputmethod.InputMethodManager;
@@ -45,6 +46,25 @@ public class PinCodeView extends LinearLayout {
 	private int mImeActionId;
 	private CharSequence mImeActionLabel;
 
+	private OnEditorActionListener mOnEditorActionListener;
+
+	/**
+	 * Interface definition for a callback to be invoked when an action is performed on the editor.
+	 */
+	public interface OnEditorActionListener {
+		/**
+		 * Called when an action is being performed.
+		 * 
+		 * @param view
+		 *            The view for which the editor action was invoked
+		 * @param actionCode
+		 *            Identifier of the action. This will be either the identifier you supplied, or
+		 *            EditorInfo.IME_NULL if being called due to the enter key being pressed.
+		 * @return Return true if you have consumed the action, else return false
+		 */
+		public boolean onEditorAction(PinCodeView view, int actionCode);
+	}
+
 	public PinCodeView(Context context) {
 		super(context);
 	}
@@ -62,9 +82,9 @@ public class PinCodeView extends LinearLayout {
 
 	private void init(Context context, AttributeSet attrs, int defStyle) {
 		Log.d(TAG, "[init]");
-		
+
 		int maxPinLength = DEFAULT_PIN_LENGTH;
-		
+
 		TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.PinCodeView, defStyle, 0);
 		try {
 			final int N = a.getIndexCount();
@@ -120,9 +140,9 @@ public class PinCodeView extends LinearLayout {
 
 	public void setMaxPinLength(int newLength) {
 		if (mMaxPinLength != newLength) {
-			mMaxPinLength = newLength;			
+			mMaxPinLength = newLength;
 			mPinText.setFilters(new InputFilter[] { new InputFilter.LengthFilter(newLength) });
-			
+
 			CharSequence text = mPinText.getText();
 			if (text != null && text.length() > newLength) {
 				mPinText.setText(text.subSequence(0, newLength));
@@ -188,6 +208,10 @@ public class PinCodeView extends LinearLayout {
 	public int getImeActionId() {
 		return mImeActionId;
 	}
+	
+	public void setOnEditorActionListener(OnEditorActionListener listener) {
+		mOnEditorActionListener = listener;
+	}
 
 	@Override
 	public boolean performClick() {
@@ -214,29 +238,87 @@ public class PinCodeView extends LinearLayout {
 	public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
 		Log.d(TAG, "[onCreateInputConnection]");
 
-		PinCodeInputConnection connection = new PinCodeInputConnection(this);
-
 		outAttrs.inputType = mInputType;
 		outAttrs.imeOptions = mImeOptions;
 		outAttrs.actionLabel = mImeActionLabel;
 		outAttrs.actionId = mImeActionId;
+		outAttrs.initialSelStart = Selection.getSelectionStart(mPinText.getText());
+		outAttrs.initialSelEnd = Selection.getSelectionEnd(mPinText.getText());
 
+		if (focusSearch(FOCUS_DOWN) != null) {
+			outAttrs.imeOptions |= EditorInfo.IME_FLAG_NAVIGATE_NEXT;
+		}
+		if (focusSearch(FOCUS_UP) != null) {
+			outAttrs.imeOptions |= EditorInfo.IME_FLAG_NAVIGATE_PREVIOUS;
+		}
+		if ((outAttrs.imeOptions & EditorInfo.IME_MASK_ACTION) == EditorInfo.IME_ACTION_UNSPECIFIED) {
+			if ((outAttrs.imeOptions & EditorInfo.IME_FLAG_NAVIGATE_NEXT) != 0) {
+				// An action has not been set, but the enter key will move to the next focus, so set
+				// the editor action to that.
+				outAttrs.imeOptions |= EditorInfo.IME_ACTION_NEXT;
+			} else {
+				// An action has not been set and there is no focus to move to
+				outAttrs.imeOptions |= EditorInfo.IME_ACTION_DONE;
+			}
+		}
+
+		PinCodeInputConnection connection = new PinCodeInputConnection(this);
 		return connection;
-	}
-
-	public KeyListener getKeyListener() {
-		Log.d(TAG, "[getKeyListener]");
-		return mPinText.getKeyListener();
 	}
 
 	public void onEditorAction(int actionCode) {
 		Log.d(TAG, "[onEditorAction]");
+		mPinText.onEditorAction(actionCode);
+
+		if (mOnEditorActionListener != null) {
+			if (mOnEditorActionListener.onEditorAction(this, actionCode)) {
+				return;
+			}
+		}
+
+		// Default handling for some standard actions
+		if (actionCode == EditorInfo.IME_ACTION_NEXT) {
+			View v = focusSearch(FOCUS_FORWARD);
+			if (v != null) {
+				if (!v.requestFocus(FOCUS_FORWARD)) {
+					throw new IllegalStateException("focus search returned a view "
+							+ "that wasn't able to take focus!");
+				}
+			}
+			return;
+
+		} else if (actionCode == EditorInfo.IME_ACTION_PREVIOUS) {
+			View v = focusSearch(FOCUS_BACKWARD);
+			if (v != null) {
+				if (!v.requestFocus(FOCUS_BACKWARD)) {
+					throw new IllegalStateException("focus search returned a view "
+							+ "that wasn't able to take focus!");
+				}
+			}
+			return;
+
+		} else if (actionCode == EditorInfo.IME_ACTION_DONE) {
+			InputMethodManager imm =
+					(InputMethodManager) getContext()
+						.getSystemService(Context.INPUT_METHOD_SERVICE);
+			if (imm != null && imm.isActive(this)) {
+				imm.hideSoftInputFromWindow(getWindowToken(), 0);
+			}
+			return;
+		}
+
+		// unhandled action; pass it to the TextView
 		mPinText.onEditorAction(actionCode);
 	}
 
 	public void onPrivateIMECommand(String action, Bundle data) {
 		Log.d(TAG, "[onPrivateIMECommand]");
 		mPinText.onPrivateIMECommand(action, data);
+	}
+
+	public KeyListener getKeyListener() {
+		Log.d(TAG, "[getKeyListener]");
+		return mPinText.getKeyListener();
 	}
 
 	@Override
@@ -280,14 +362,14 @@ public class PinCodeView extends LinearLayout {
 	protected Parcelable onSaveInstanceState() {
 		Log.d(TAG, "[onSaveInstanceState]");
 		Parcelable superState = super.onSaveInstanceState();
-		
+
 		CharSequence text = mPinText.getText();
 		if (!TextUtils.isEmpty(text)) {
 			SavedState ss = new SavedState(superState);
 			ss.text = text;
 			return ss;
 		}
-		
+
 		return superState;
 	}
 
@@ -301,16 +383,16 @@ public class PinCodeView extends LinearLayout {
 
 		SavedState ss = (SavedState) state;
 		super.onRestoreInstanceState(ss.getSuperState());
-		
+
 		if (ss.text != null) {
 			mPinText.setText(ss.text);
 			Selection.setSelection(mPinText.getEditableText(), ss.text.length());
-        }
+		}
 	}
-	
+
 	public static class SavedState extends BaseSavedState {
 		private CharSequence text;
-		
+
 		public SavedState(Parcelable superState) {
 			super(superState);
 		}
@@ -319,23 +401,23 @@ public class PinCodeView extends LinearLayout {
 			super(source);
 			text = TextUtils.CHAR_SEQUENCE_CREATOR.createFromParcel(source);
 		}
-		
+
 		@Override
 		public void writeToParcel(Parcel dest, int flags) {
 			Log.d(TAG, "[writeToParcel]");
 			super.writeToParcel(dest, flags);
 			TextUtils.writeToParcel(text, dest, flags);
 		}
-		
+
 		public static final Parcelable.Creator<SavedState> CREATOR =
 				new Parcelable.Creator<SavedState>() {
-			public SavedState createFromParcel(Parcel in) {
-				return new SavedState(in);
-			}
+					public SavedState createFromParcel(Parcel in) {
+						return new SavedState(in);
+					}
 
-			public SavedState[] newArray(int size) {
-				return new SavedState[size];
-			}
-		};
+					public SavedState[] newArray(int size) {
+						return new SavedState[size];
+					}
+				};
 	}
 }
